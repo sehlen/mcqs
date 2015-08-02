e630ff67684cc94e64208334c3f4bd63b642c7c2
Stephan Ehlen <stephan.j.ehlen@gmail.com>
2015-08-02 11:27:40 -0400

    Use click.File

--- mcqs/cli.py
+++ mcqs/cli.py
@@ -4,11 +4,11 @@ import click
 @click.command()
 @click.option('--num-versions', '-n', default=2, help='The number of versions')
 @click.option('--shuffle-problems', '-sp', is_flag=True, help='Also shuffle problems')
-@click.argument('filename', default='exam.tex', required=True)
+@click.argument('examfile', type=click.File('r'), required=True)
 
-def main(filename, num_versions, shuffle_problems):
+def main(examfile, num_versions, shuffle_problems):
     """A multiple choice question shuffler"""
-    shuffle_mcqs(filename, num_versions, shuffle_problems)
+    shuffle_mcqs(examfile, num_versions, shuffle_problems)
 
 import re, os
 from random import shuffle
@@ -19,19 +19,19 @@ def shuffle_items(p):
     s = re.sub(r'(\\item.*)',lambda m: items.pop(), p.group(0))
     return s
 
-def shuffle_mcqs(filename, num_versions=2, shuffle_problems=False):
+def shuffle_mcqs(examfile, num_versions=2, shuffle_problems=False):
     versions = range(1,num_versions+1)
-    with open (filename, "r") as f:
-        exam = f.read()
-        for v in versions:
-            exam_v = re.sub("Version XX","Version " + str(v), exam)
-            exam_v = re.sub("version XX","version " + str(v), exam_v)
-            exam_v = re.sub(r'%BeginMCItems(.*?)%EndMCItems', shuffle_items, exam_v, flags=re.DOTALL|re.MULTILINE)
-            problems = re.findall(r'%BeginMCProblem(.*?)%EndMCProblem', exam_v, flags=re.DOTALL|re.MULTILINE)
-            shuffle(problems)
-            exam_v = re.sub(r'%BeginMCProblem(.*?)%EndMCProblem', lambda m: problems.pop(), exam_v, flags=re.DOTALL|re.MULTILINE)
-            fvn = filename[:-4] + "-" + str(v) + ".tex"
-            fv = open(fvn, 'w')
-            fv.write(exam_v)
-            fv.close()
-            click.echo("Wrote " + os.path.abspath(fv.name))
+    exam = examfile.read()
+    for v in versions:
+        exam_v = re.sub("Version XX","Version " + str(v), exam)
+        exam_v = re.sub("version XX","version " + str(v), exam_v)
+        exam_v = re.sub(r'%BeginMCItems(.*?)%EndMCItems', shuffle_items, exam_v, flags=re.DOTALL|re.MULTILINE)
+        problems = re.findall(r'%BeginMCProblem(.*?)%EndMCProblem', exam_v, flags=re.DOTALL|re.MULTILINE)
+        shuffle(problems)
+        exam_v = re.sub(r'%BeginMCProblem(.*?)%EndMCProblem', lambda m: problems.pop(), exam_v, flags=re.DOTALL|re.MULTILINE)
+        fvn = examfile.name[:-4] + "-" + str(v) + ".tex"
+        fv = open(fvn, 'w')
+        fv.write(exam_v)
+        fv.close()
+        click.echo("Wrote " + os.path.abspath(fv.name))
+    examfile.close()
