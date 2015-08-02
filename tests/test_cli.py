e54cb870c35a8f362499c583247d3c6b9c9192e8
Stephan Ehlen <stephan.j.ehlen@gmail.com>
2015-08-02 12:09:42 -0400

    Added simple example and tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,4 @@
-import pytest
+import pytest, os
 from click.testing import CliRunner
 from mcqs import cli
 
@@ -8,11 +8,20 @@ def runner():
     return CliRunner()
 
 
-## def test_cli(runner):
-##     result = runner.invoke(cli.main)
-##     assert result.exit_code == 0
-##     assert not result.exception
-##     assert result.output.strip() == 'Hello, world.'
+def test_cli(runner):
+    result = runner.invoke(cli.main, ['-n2', 'examples/test-exam.tex'])
+    assert result.exit_code == 0
+    assert not result.exception
+    fn1 = 'examples/test-exam-1.tex'
+    f1 = open(fn1, 'r')
+    fn1 = os.path.abspath(f1.name)
+    f1.close()
+    os.remove(fn1)
+    fn2 = 'examples/test-exam-2.tex'
+    f2 = open(fn2, 'r')
+    fn2 = os.path.abspath(f2.name)
+    f2.close()
+    os.remove(fn2)
 
 
 ## def test_cli_with_option(runner):
